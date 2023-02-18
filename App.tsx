77e2e8d4434dd5879d46bf7bdff81d0fb136367f
ismaelsousa <ismaelsousa1998@outlook.com>
2023-02-18 10:47:57 -0300

    feat: add NotFound component

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import BackButton from "./src/common/components/BackButton";
 import Container from "./src/common/components/Container";
 import Content from "./src/common/components/Content";
 import Icon from "./src/common/components/Icon";
+import NotFound from "./src/common/components/NotFound";
 import Spacer from "./src/common/components/Spacer";
 import Text from "./src/common/components/Text";
 import { defaultTheme } from "./src/common/constants/styles/theme/defaultTheme";
@@ -22,19 +23,16 @@ export default function App() {
             }}
           />
           <Spacer height={20} />
-          <Text size={30} color="onSecondary">
-            1 Meu componente de text
-          </Text>
-          <Icon icon="home" color="red" size={40}></Icon>
-          <Spacer height={30} />
+
           <Text size={30} color="onSecondary">
             2 Meu componente de text
           </Text>
           <Avatar />
           <Avatar
-            size="large"
+            size="small"
             url="https://br.web.img3.acsta.net/pictures/18/08/01/20/13/3535250.jpg"
           />
+          <NotFound />
         </Content>
       </Container>
     </ThemeProvider>
---
+++ src/common/components/NotFound/types.ts
@@ -0,0 +1,7 @@
+import { NameIconTypes } from "../../constants/icons";
+
+export interface NotFoundProps {
+  title?: string;
+  icon?: NameIconTypes;
+  color?: string;
+}
