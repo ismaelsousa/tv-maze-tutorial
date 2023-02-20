7ca488c81162c839b1a2c016d31315eda62f7a3e
ismaelsousa <ismaelsousa1998@outlook.com>
2023-02-20 13:47:11 -0300

    rn-27

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "styled-components/native";
 
 import { defaultTheme } from "./src/common/constants/styles/theme/defaultTheme";
+import FavoritesShows from "./src/common/contexts/FavoritesShows";
 import Routes from "./src/navigation";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <NavigationContainer>
-        <Routes />
+        <FavoritesShows>
+          <Routes />
+        </FavoritesShows>
       </NavigationContainer>
     </ThemeProvider>
   );
